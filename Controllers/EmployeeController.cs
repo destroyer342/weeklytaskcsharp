84abbba542af99faa08ca2b1be9acb9ba2d3271b
Rance Cañete <rancedestroyer2@gmail.com>
2022-11-14 14:04:32 +0800

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WeeklyAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+            return new string[] { "Employee 1", "Employee 2" };
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (id == 1)
+            {
+                try
+                {
+                    return "You input " + id;
+                }
+                catch (HttpRequestException)
+                {
+
+                    return "Retrun something";
+                }
+            }
+            return "Wrong Input";
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
